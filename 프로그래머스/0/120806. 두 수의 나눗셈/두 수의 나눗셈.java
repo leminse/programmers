d4066413aa9d26013d915b142829212e8498de98
이민서 <134268275+leminse@users.noreply.github.com>
2024-08-31 22:23:23 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double answer = 0;
+        
+        answer = (double) num1 / num2 * 1000;
+        
+        return (int)answer;
+        
+        //강제형 변환
+    }
+}
